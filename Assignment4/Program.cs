7100fb01ed36506ff0de5904da6b5a10d7d8290c
AlshaimaaGamal <shaimaa.20377393@compit.aun.edu.eg>
2025-07-12 09:45:05 +0200

    Assignment 3 Q10

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -160,13 +160,13 @@
             #region Q9
             //9.Write a program that calculates the Body Mass Index (BMI) given a person's weight in kilograms and height in meters. 
 
-           // Console.WriteLine("Enter the weight ");
-           //int.TryParse(Console.ReadLine() ,out int Weight);
-           // Console.WriteLine("Enter the Height ");
-           // int.TryParse(Console.ReadLine(), out int Height);
+            // Console.WriteLine("Enter the weight ");
+            //int.TryParse(Console.ReadLine() ,out int Weight);
+            // Console.WriteLine("Enter the Height ");
+            // int.TryParse(Console.ReadLine(), out int Height);
 
-           // int BMI = Weight/(Height*Height) ;
-           // Console.WriteLine($" BMI = {BMI} K/m");
+            // int BMI = Weight/(Height*Height) ;
+            // Console.WriteLine($" BMI = {BMI} K/m");
 
 
 
@@ -187,6 +187,18 @@
 
 
 
+
+
+            #endregion
+
+            #region Q10
+
+            //10.Write a program that uses the ternary operator to check if the temperature is too hot, too cold, or just good. Assign the result in a variable then display the result. 
+
+            //Console.WriteLine("Enter the ternary ");
+            // int tempary  = Convert.ToInt32(Console.ReadLine());
+            //string result = tempary < 10 ? "cold" : tempary > 30 ? "Hot" : "Just Good";
+            //Console.WriteLine(result);  
 
 
             #endregion
