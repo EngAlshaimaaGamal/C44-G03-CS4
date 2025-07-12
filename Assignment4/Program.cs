910bb474c5a8aedf23189f85b8535b91428085b6
AlshaimaaGamal <shaimaa.20377393@compit.aun.edu.eg>
2025-07-12 08:18:46 +0200

    Assignment3 q1

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Q1
+            //1.Write a program that allows the user to enter a number then print it.
+
+            //Console.WriteLine("Enter the number ");
+            //int.TryParse(Console.ReadLine(), out int number);
+            //Console.WriteLine($"The Number : {number}");
+            //Console.ReadKey();
+
+
+            #endregion
 
 
 
